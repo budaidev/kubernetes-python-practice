f8f7e46147e22dda32a7d65e9b9cfd154637712e
budaidev <53112929+budaidev@users.noreply.github.com>
2024-11-06 06:13:56 +0100

    update

---
+++ terraform/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
